8489b93c33650e294df01c96b25bd92101e273dc
bion0625 <bion0625@gmail.com>
2024-03-22 15:27:37 +0900

    포스팅 삭제 테스트 코드 작성

--- src/main/java/com/fastcampus/sns/service/PostService.java
+++ src/main/java/com/fastcampus/sns/service/PostService.java
@@ -42,4 +42,19 @@ public class PostService {
         postEntity.setBody(body);
         return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
+
+    public void delete(String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUNT, String.format("%s not founded", userName)));
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        // post permission
+        if(postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
+        }
+
+        postEntityRepository.delete(postEntity);
+    }
 }
